2eded12c2dca9e287a38e6e01bcba5271c0665d7
alwaysmohawk <77303173+alwaysmohawk@users.noreply.github.com>
2023-03-09 22:22:35 -0500

    setting up i guess

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include <Arduino.h>
+#include "wifi.h"
+#include "wificred.h"
 
 void setup() {
   // put your setup code here, to run once:
+  /*create a wificred.h file like this:
+   * const char* ssid = "ssid";
+   * const char* password = "password";
+   */
 }
 
 void loop() {
