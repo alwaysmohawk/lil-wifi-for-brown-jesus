4bb65b210eb005a6b1713a7788d197c70300c09c
alwaysmohawk <77303173+alwaysmohawk@users.noreply.github.com>
2023-03-09 22:30:57 -0500

    added serial

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,29 @@ void setup() {
    * const char* ssid = "ssid";
    * const char* password = "password";
    */
+  WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    
+    Serial.begin(115200);
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(15, HIGH);
+  delay(500);
+  digitalWrite(15, LOW);
+  delay(500);
+
 }
